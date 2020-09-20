20395abd29d483041d5d8503e135afe16cd316a4
AmarSivas <amarsivas.b@gmail.com>
2020-09-20 15:58:15 -0400

    intial changes for volatile  example
    intial changes for volatile  example

--- src/VolatileExampleTest.java
+++ src/VolatileExampleTest.java
@@ -1,5 +1,30 @@
-public class VolatileExampleTest {
+public class VolatileExampleTest implements Runnable {
+    SharedData obj = new SharedData();
+
     public static void main(String[] args) {
-        System.out.println("tests");
+        VolatileExampleTest vd = new VolatileExampleTest();
+        new Thread(vd).start();
+        new Thread(vd).start();
+        new Thread(vd).start();
+        new Thread(vd).start();
+    }
+
+    @Override
+    public void run() {
+        obj.incrementCounter();
+        System.out.println("Counter for Thread " + Thread.currentThread().getName() +
+                " " + obj.getCounter());
+    }
+}
+
+class SharedData {
+    public volatile int sharedVolatileObject = 0;
+
+    public int getCounter() {
+        return sharedVolatileObject;
+    }
+
+    public void incrementCounter() {
+        ++sharedVolatileObject;
     }
 }
